0932e2458e9b2b6a4717cfe83db5f9aa5d95cec3
bobikSR <bobiksr123@gmail.com>
2024-03-18 17:56:57 +0100

    added overlay + hashing versions

--- VersionRoulette.py
+++ VersionRoulette.py
@@ -1,40 +1,32 @@
-import random
+
 import tkinter as tk
 
+from methods import roll_versions, update_overlay, hash_versions
+
 # todo: link a very simple livesplit layout - version 1 - 5, big timer
-# make that a button
 # todo: add link to submission form + make a lb on google sheets
 
+rolled_versions: str = ""
 
-
-version_list = ["1.0", "1.1", "1.2", "1.3", "1.4", "1.5", "1.6", "1.7", "1.8", "1.9", "1.10",
-                "1.11", "1.12", "1.13", "1.14", "1.15", "1.16", "1.17", "1.18", "1.19", "1.19",
-                "1.20"]
-
-
-def roll_versions() -> list[str]:
-    possible_versions = version_list.copy()
-    rolled_versions = [str]
-    for i in range(5):
-        version = possible_versions[random.randint(1, len(possible_versions) - 1)]
-        rolled_versions.append(version)
-        possible_versions.remove(version)
-    return rolled_versions
-
-
-def get_versions():
-    string = "Rolled versions:"
+def get_new_versions():
+    title_str = "Rolled versions:"
     versions = roll_versions()
+    versions_str = ""
     for i in versions[1:]:
-        string += f"\n{i}"
-    label.config(text=string)
+        versions_str += f"\n{i}"
+    global rolled_versions
+    rolled_versions = versions_str
+    label.config(text=f"{title_str}{versions_str}")
+    update_overlay(versions_str)
 
 
 root = tk.Tk()
-root.geometry("200x250")
+root.geometry("200x200")
 root.title("Version Roulette")
-roll = tk.Button(root, text="Spin the roulette!", command=get_versions)
-roll.pack()
+roll = tk.Button(root, text="Spin the roulette!", command=get_new_versions)
+roll.pack(pady=(10,0))
 label = tk.Label(root, text="")
 label.pack()
+submit_versions = tk.Button(root, text="submit versions", command=hash_versions(rolled_versions), anchor="s")
+submit_versions.pack(side="bottom", pady=(10,20))
 root.mainloop()
---
+++ methods.py
@@ -0,0 +1,33 @@
+import hashlib
+
+from PIL import Image, ImageDraw,ImageFont
+import random
+
+
+version_list = ["1.0", "1.1", "1.2", "1.3", "1.4", "1.5", "1.6", "1.7", "1.8", "1.9", "1.10",
+                "1.11", "1.12", "1.13", "1.14", "1.15", "1.16", "1.17", "1.18", "1.19", "1.19",
+                "1.20"]
+
+
+def update_overlay(versions_str: str):
+    overlay_str = f"Versions:{versions_str}"
+    img = Image.new('RGB', (255, 255), color='green')
+    overlay = ImageDraw.Draw(img)
+    overlay.text((10,10), overlay_str, font=ImageFont.truetype('arial.ttf', size=20))
+    img.save('overlay.png')
+
+
+def roll_versions() -> list[str]:
+    possible_versions = version_list.copy()
+    rolled_versions = [str]
+    for i in range(5):
+        version = possible_versions[random.randint(1, len(possible_versions) - 1)]
+        rolled_versions.append(version)
+        possible_versions.remove(version)
+    return rolled_versions
+
+
+def hash_versions(versions_str: str):
+    hashed_versions = hashlib.sha256(versions_str.encode()).hexdigest()
+    with open('versions.txt', 'w') as versions_file:
+        versions_file.write(hashed_versions)
